46a76144fe6974bedc3d89a40f1f94adb7a35c6d
Angel Jair Neri Huerta <yayodcv@gmail.com>
2024-10-30 17:32:09 -0600

    Interaction <OrderCard> Delete products

--- src/components/OrderCard/index.jsx
+++ src/components/OrderCard/index.jsx
@@ -1,6 +1,16 @@
+import { useAppContext } from '@/contexts';
 import { XMarkIcon } from '@heroicons/react/24/solid';
 
 const OrderCard = ({ data }) => {
+	const { cart } = useAppContext();
+
+	const handleDelete = id => {
+		const filteredProducts = cart?.cardProducts?.filter(
+			product => product.id != id
+		);
+		cart?.setcardProducts(filteredProducts);
+	};
+
 	return (
 		<div className=" relative flex gap-2 m-2 p-4 w-6/6 rounded-lg 	   bg-gray-300">
 			<figure className="relative w-3/6 h-22 ">
@@ -14,7 +24,10 @@ const OrderCard = ({ data }) => {
 				<span className="text-sm font-light line-clamp-3">{data?.title}</span>
 				<span className="text-lg font-medium">${data?.price}</span>
 			</p>
-			<XMarkIcon className="h-6 w-6 text-black cursor-pointer " />
+			<XMarkIcon
+				className="h-6 w-6 text-black cursor-pointer "
+				onClick={() => handleDelete(data?.id)}
+			/>
 		</div>
 	);
 };
